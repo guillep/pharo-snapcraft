e1472d332b0279cf2ff0229be8cc76b636217964
guille <guillermopolito@gmail.com>
2017-03-28 17:37:37 +0200

    updating to use open smalltalk vm

--- snap/plugins/x-pharo-build-plugin.py
+++ snap/plugins/x-pharo-build-plugin.py
@@ -4,7 +4,7 @@ import os
 
 class PharoBuildPlugin(snapcraft.BasePlugin):
 	def build(self):
-		result = self.run(['scripts/build.sh'])
+		result = self.run(['mvm'], cwd='build.linux32x86/pharo.cog.spur/build/')
 		vmresultsdir = os.path.join(self.builddir, "results")
 		shutil.move(vmresultsdir, self.installdir)
 		return result
